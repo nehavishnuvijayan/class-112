387f15abd68820a47985cd2b02ddb98fc0259e82
nehavishnuvijayan <87350604+nehavishnuvijayan@users.noreply.github.com>
2023-01-12 21:03:16 +0530

    Add files via upload

---
+++ class 112 python.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+source="D:/nv"
+destination="D:/abc"
+files=os.listdir(source)
+print(files)
+for i in files:
+    name,ext=os.path.splitext(i)
+    if ext==" ":
+        continue
+    if ext in ['.txt','.doc','.docx','.pdf']:
+        path1 = source + '/' + i
+        path2 = destination + '/' + "document_files"
+        path3 = destination + '/' + "document_files" + '/' + i
+    if os.path.exists(path2):
+        print("Moving")
+        shutil.move(path1,path3)
+    else:
+        os.makedirs(path2)
+        print("Moving")
+        shutil.move(path1,path3)
+'''
+import os
+import shutil
+source="D:/abc"
+destination="C:\Users\User\OneDrive"
+files=os.listdir(source)
+print(files)
+for i in files:
+    name,ext=os.path.splitext(i)
+    if ext==" ":
+        continue
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        path1 = source + '/' + i
+        path2 = destination + '/' + "Image_Files"
+        path3 = destination + '/' + "Image_Files" + '/' + i
+    if os.path.exists(path2):
+        print("Moving")
+        shutil.move(path1,path3)
+    else:
+        os.makedirs(path2)
+        print("Moving")
+        shutil.move(path1,path3)
+'''
